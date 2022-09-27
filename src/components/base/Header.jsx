e9bc680317e4f4bec1682c36585b34a4a9571e88
Einstein-Junior <adedejitobiloba7@gmail.com>
2022-09-27 13:04:43 +0100

    finished work on the mobile navbar

--- src/components/base/Header.jsx
+++ src/components/base/Header.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import logo from "../../Assets/logo.svg";
 import { GiHamburgerMenu } from "react-icons/gi";
-// import {}
+import { useState } from "react";
+import {Link} from "react-router-dom"
 
 function Header() {
   // I essentially want an header that has my logo on the left side and an hamburger on the right hand side
-  const style = {width: "27px", height: "27px", borderRadius: '50%', color: '#fff'};
+  const [navOpen, setNavOpen] = useState(false);
+
+  const openNav = () => {
+    setNavOpen(!navOpen);
+  };
+
+  const style = {
+    width: "27px",
+    height: "27px",
+    borderRadius: "50%",
+    color: "#fff",
+  };
   return (
     <header className="bg-[#023E8A]">
       <nav className="relative w-[90%] max-w-[900px] my-0 mx-auto py-[.7em]">
@@ -16,18 +28,28 @@ function Header() {
             className="w-[60px] h-[60px] object-contain"
           />
         </div>
-        <div className="right-top absolute top-[1.7em] right-[1em]">
-          <GiHamburgerMenu style={style} />
+        <div className="right-top absolute top-[1.7em] right-[.4em]">
+          <GiHamburgerMenu style={style} onClick={openNav} />
+        </div>
+        {navOpen && (
           <div className="my-sidebar-as-role">
             {/* I want the nav bar to show up from the entire top or it could be a sidebar */}
             <ul className="list">
-              <li>About</li>
-              <li>Skills</li>
-              <li>Work</li>
-              <li>Contact</li>
+              <Link to="/">
+                <li className="list-item">About</li>
+              </Link>
+              <Link to="/skills">
+                <li className="list-item">Skills</li>
+              </Link>
+              <Link to="/works">
+                <li className="list-item">Work</li>
+              </Link>
+              <Link to="/contact">
+                <li className="list-item">Contact</li>
+              </Link>
             </ul>
           </div>
-        </div>
+        )}
       </nav>
     </header>
   );
